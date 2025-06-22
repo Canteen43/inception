dd5fddc607dbc232d74f6d94ad3513d7abc97d05
Canteen43 <karl.weihmann@gmail.com>
2025-06-22 12:44:54 +0200

    quick commit

--- wordpressinstall.php
+++ wordpressinstall.php
@@ -1,17 +1,15 @@
 <?php
 
-if (php_sapi_name() === 'cli') {
-    $_SERVER['HTTP_HOST'] = 'localhost';
-    $_SERVER['REQUEST_URI'] = '/';
-}
-
+define('WP_HOME', 'https://kweihman.42.fr');
+define('WP_SITEURL', 'https://kweihman.42.fr');
 define('WP_INSTALLING', true);
+
 require_once __DIR__ . '/wp-load.php';
 require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
 try {
     $result = wp_install(
-        'Karls Saturday Screw Up 2',       // Site title
+        'Karls Secure Saturday Screw Up',       // Site title
         'user1',               // Admin username
         'user1@example.com',   // Admin email
         true,                  // Public site
@@ -20,7 +18,6 @@ try {
     );
 
     if ( is_wp_error($result) ) {
-        // Installation failed, show error message
         echo "Installation failed: " . $result->get_error_message() . "\n";
         exit(1);
     } else {
