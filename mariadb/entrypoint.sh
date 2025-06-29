965df373e6288fec6f47f4a544634d905e0f3efa
KarlWSL <karl.weihmann@gmail.com>
2025-06-29 23:31:31 +0200

    bla

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -5,10 +5,13 @@ mariadbd --user=mysql &
 
 # wait for the database to start
 # TODO: find better alternative to sleep (maybe ping, maybe error on delay?)
-sleep 1
+sleep 3
 
 # create the database and user for WordPress
 mariadb < /root/setup_wordpress_on_mariadb.sql
+
+# wait for command to run
+sleep 1
 
 # stop mariadbd so it can be restarted in the foreground
 pkill mariadbd
