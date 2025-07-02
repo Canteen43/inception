1328f4b75710c1df488784fab78bc54ec0b9f9e1
KarlWSL <karl.weihmann@gmail.com>
2025-07-02 13:43:35 +0200

    added volume to wordpress container

--- wordpress-php/entrypoint.sh
+++ wordpress-php/entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Copying to the wordpress volume if it is not initialized
+if [ ! -f /var/www/wordpress/.initialized ]; then
+	echo "Entrypoint script: Initializing volume..."
+	cp -r /var/www/wordpress_src/* /var/www/wordpress/
+	touch /var/www/wordpress/.initialized
+else
+	echo "Entrypoint script: Volume already initialized..."
+fi
+
+# Deleting wordpress_src
+echo "Entrypoint script: Deleting wordpress_src directory..."
+rm -rf /var/www/wordpress_src
+
 # Wait until MariaDB is ready
 echo "Entrypoint script: Waiting for MariaDB to be ready..."
 until mariadb-admin ping -h mariadb -P 3306 -u wpuser -p42; do
@@ -8,7 +21,6 @@ until mariadb-admin ping -h mariadb -P 3306 -u wpuser -p42; do
 done
 echo "Entrypoint script: MariaDB is ready."
 
-# TODO: Could not be opened
 # Run the WordPress installation script
 echo "Entrypoint script: Running WordPress installation script..."
 php /var/www/wordpress/wordpressinstall_localhost.php
