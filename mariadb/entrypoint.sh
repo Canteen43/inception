c5df1db7cfa89583d7280c6a3066b3ad5d92a7b7
KarlWSL <karl.weihmann@gmail.com>
2025-07-05 14:05:33 +0200

    database volume now works for fresh start and restart

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,31 +1,48 @@
 #!/bin/sh
 
-# Initializing the database (this puts system tables into mariadb)
-mariadb-install-db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
+# Checking if initialization is needed. (Initialization puts system tables into mariadb)
+echo "Entrypoint script: Checking if MariaDB needs to be initialized..."
+if [ ! -d /var/lib/mysql/mysql ]; then
+	echo "Entrypoint script: /var/lib/mysql/mysql does not exist. Initializing MariaDB..."
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
+	echo "Entrypoint script: Initialization complete."
+else
+	echo "Entrypoint script: /var/lib/mysql/mysql already exists. Skipping initialization."
+fi
 
-# Starting mariadb daemon in the background
-echo "Entrypoint script: Starting mariadbd in the background..."
-mariadbd --user=mysql &
+# Checking if wordpress table and user are installed 
+echo "Entrypoint script: Checking if wordpress table and user are already added..."
+if [ ! -d /var/lib/mysql/wordpress ]; then
+	echo "Entrypoint script: /var/lib/mysql/wordpress does not exist."
+	
+	# Starting mariadb daemon in the background. (MariaDB needs to be running to create the database and user)
+	echo "Entrypoint script: Starting mariadbd in the background..."
+	mariadbd --user=mysql &
 
-# Wait until MariaDB is ready
-echo "Entrypoint script: Waiting for MariaDB to be ready..."
-until mariadb-admin ping; do
-    echo "Entrypoint script: MariaDB is not ready yet. Waiting 0.1 seconds..."
-    sleep 0.1
-done
-echo "Entrypoint script: MariaDB is ready."
+	# Waiting until MariaDB is ready
+	echo "Entrypoint script: Waiting for MariaDB to be ready..."
+	until mariadb-admin ping; do
+		echo "Entrypoint script: MariaDB is not ready yet. Waiting 0.1 seconds..."
+		sleep 0.1
+	done
+	echo "Entrypoint script: MariaDB is ready."
 
-# create the database and user for WordPress
-echo "Entrypoint script: Creating WordPress database and user..."
-mariadb -e "
-CREATE DATABASE wordpress;
-CREATE USER 'wpuser'@'%' IDENTIFIED BY '42';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';
-"
+	# Creating the database and user for WordPress
+	echo "Entrypoint script: Creating WordPress database and user..."
+	mariadb -e "
+	CREATE DATABASE wordpress;
+	CREATE USER 'wpuser'@'%' IDENTIFIED BY '42';
+	GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'%';
+	"
+	# Stopping mariadb background daemon
+	echo "Entrypoint script: Stopping MariaDB daemon..."
+	pkill mariadbd
 
-# stop mariadbd so it can be restarted in the foreground
-echo "Entrypoint script: Stopping MariaDB daemon..."
-pkill mariadbd
+	echo "Entrypoint script: WordPress database and user created."
+else
+	echo "Entrypoint script: /var/lib/mysql/wordpress already exists. Skipping database and user creation."
+fi
+
 
 # replaces the shell process with CMD
 echo "Entrypoint script: Replacing shell process with CMD: $@"
--- wordpress-php/entrypoint.sh
+++ wordpress-php/entrypoint.sh
@@ -13,7 +13,7 @@ fi
 echo "Entrypoint script: Deleting wordpress_src directory..."
 rm -rf /var/www/wordpress_src
 
-# Wait until MariaDB is ready
+# Waiting until MariaDB is ready
 echo "Entrypoint script: Waiting for MariaDB to be ready..."
 until mariadb-admin ping -h mariadb -P 3306 -u wpuser -p42; do
     echo "Entrypoint script: MariaDB is not ready yet. Waiting 0.1 seconds..."
@@ -21,9 +21,14 @@ until mariadb-admin ping -h mariadb -P 3306 -u wpuser -p42; do
 done
 echo "Entrypoint script: MariaDB is ready."
 
-# Run the WordPress installation script
-echo "Entrypoint script: Running WordPress installation script..."
-php /var/www/wordpress/wordpressinstall_localhost.php
+# Checking if wordpress needs installation
+echo "Entrypoint script: Checking if wordpress needs installation..."
+if mariadb -h mariadb -u wpuser -p42 -e "USE wordpress; SELECT * FROM wp_options;"  >/dev/null 2>&1; then
+    echo "Entrypoint script: WordPress already installed, skipping installation."
+else
+    echo "Entrypoint script: WordPress not installed, running installer..."
+    php /var/www/wordpress/wordpressinstall_localhost.php
+fi
 
 # replaces the shell process with CMD
 echo "Entrypoint script: Replacing shell process with CMD: $@"
