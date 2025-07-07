3032228397417089a88b12ed5d305e68b77316a2
KarlWSL <karl.weihmann@gmail.com>
2025-07-07 17:18:25 +0200

    removed sleep from nginx entrypoint

--- src/nginx/entrypoint.sh
+++ src/nginx/entrypoint.sh
@@ -14,17 +14,6 @@ else
 	echo "Entrypoint script: Key and cert already exist..."
 fi
 
-
-# TODO: Check connection acceptance of wordpress-php container
-# Copying to the wordpress volume if it is not initialized
-# echo "Entrypoint script: Waiting for Wordpress to be ready..."
-# if [ ! -f /var/www/wordpress/.installed ]; then
-#     echo "Entrypoint script: Wordpress is not ready yet. Waiting 0.1 seconds..."
-#     sleep 0.1
-# fi
-# echo "Entrypoint script: Wordpress is ready."
-sleep 5
-
 # replaces the shell process with CMD
 echo "Entrypoint script: Replacing shell process with CMD: $@"
 exec "$@"
