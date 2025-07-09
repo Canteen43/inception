ca3c1f29acd7898278bea9e08674aa4f2eb9e923
Canteen43 <karl.weihmann@gmail.com>
2025-07-09 15:51:20 +0200

    added customizable hostname

--- src/wordpress-php/wordpressinstall.php
+++ src/wordpress-php/wordpressinstall.php
@@ -1,9 +1,8 @@
 <?php
 
-define('WP_HOME', 'https://kweihman.42.fr:8081');
-// define('WP_HOME', 'https://kweihman.42.fr:' . getenv('HOST_PORT'));
-define('WP_SITEURL', 'https://kweihman.42.fr:8081');
-// define('WP_SITEURL', 'https://kweihman.42.fr:' . getenv('HOST_PORT'));
+$host = 'https://' . getenv('HOST_NAME') . ':' . getenv('HOST_PORT');
+define('WP_HOME', $host);
+define('WP_SITEURL', $host);
 define('WP_INSTALLING', true);
 
 require_once __DIR__ . '/wp-load.php';
